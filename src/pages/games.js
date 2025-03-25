f6c2d333016e621e88c8b82cb46b4901c1ef79a5
PuremoonNetwork <15482908+puremoonnetwork@user.noreply.gitee.com>
2025-03-25 09:01:33 +0000

    add src/pages/games.js

---
+++ src/pages/games.js
@@ -0,0 +1,13 @@
+export const GAMES = [
+  {
+    title: "原神",
+    icon: "/img/game-icons/genshin.png",
+    url: "/games/genshin"
+  },
+  {
+    title: "王者荣耀",
+    icon: "/img/game-icons/honor-of-kings.png",
+    url: "/games/hok"
+  },
+  // 添加更多游戏...
+];
