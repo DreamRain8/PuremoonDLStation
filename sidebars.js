157f459e9b5ea6f1320c9e46e47278142b476403
PuremoonNetwork <15482908+puremoonnetwork@user.noreply.gitee.com>
2025-03-25 10:38:20 +0000

    update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -16,7 +16,7 @@ const sidebars = {
     // By default, Docusaurus generates a sidebar from the docs folder structure
     tutorialSidebar: [{
             type: 'doc',
-            id: 'games/main',
+            id: 'games',
             label: '🎮游戏下载',
         },
         {
